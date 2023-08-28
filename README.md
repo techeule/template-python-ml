ac4d570e79af626685a6e1d21a8367b78345ffef
Wassim Akachi <Wassim@BytesAndCodes.com>
2023-08-28 13:04:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# D1234X, Course Name - Lab/Project Name
+
+> Write some notes about this repo/lab/task
+
+## How to set up
+
+```shell
+python3 -m venv venv 
+
+source ./venv/bin/activate
+
+pip3 install --requirement=requirements.txt
+```
+
+## How to start and run
+
+```shell
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,21 @@
+# scipy
+import scipy
+# numpy
+import numpy
+# matplotlib
+import matplotlib
+# pandas
+import pandas
+# scikit-learn
+import sklearn
+# statsmodels
+import statsmodels
+
+print('scipy: %s' % scipy.__version__)
+print('numpy: %s' % numpy.__version__)
+print('matplotlib: %s' % matplotlib.__version__)
+print('pandas: %s' % pandas.__version__)
+print('statsmodels: %s' % statsmodels.__version__)
+print('sklearn: %s' % sklearn.__version__)
+print('\n\n')
+
